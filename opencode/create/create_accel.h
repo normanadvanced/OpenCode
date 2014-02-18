71d57d9312b92040e03953a601a30bc7a781f823
Marty Rand <marsedge95@gmail.com>
2014-02-18 14:16:39 -0600

    make create acceleration much more accurate

--- opencode/create/create_accel.h
+++ opencode/create/create_accel.h
@@ -3,6 +3,7 @@
 
 #include "create_config.h"
 #include "create_drive.h"
+#include "create_sensor.h"
 
 float speed_profile[2][15] = {
 	{0.112, 0.302, 0.492, 0.669, 0.805, 0.912, 0.977, 1.000, 0.977, 0.912, 0.805, 0.669, 0.492, 0.302, 0.112},
@@ -15,15 +16,12 @@ void create_accel_straight(int profile, float max_velocity, float distance)
 	int i;
 	float interval;
 	
-	interval = (PI * distance) / (30.0 * max_velocity);
+	interval = fabs((PI * distance) / (30.0 * max_velocity));
 	for( i = 0; i < 15; i++)
 	{
-		if(max_velocity * speed_profile[profile][i] < 50.0)
-		{
-			printf("WARNING\n%f\n", max_velocity * speed_profile[profile][i]);
-		}
 		create_straight((int)(max_velocity * speed_profile[profile][i]));
-		sleep(interval - 0.01);
+		//sleep(interval - 0.01);
+		create_wait_duration((int)(interval*10.0));
 	}
 }
 void create_accel_arc(int profile, float max_velocity, float radius, float angle)
@@ -31,11 +29,12 @@ void create_accel_arc(int profile, float max_velocity, float radius, float angle
 	int i;
 	float interval;
 	
-	interval = (PI * angle * radius * PI) / (5400.0 * max_velocity);
+	interval = fabs((PI * angle * radius * PI) / (5400.0 * max_velocity));
 	for(i = 0; i < 15; i++)
 	{
 		create_arc((int)(max_velocity * speed_profile[profile][i]), (int)radius);
-		sleep(interval - 0.01);
+		//sleep(interval - 0.01);
+		create_wait_duration((int)(interval*10.0));
 	}
 	
 }
@@ -44,11 +43,12 @@ void create_accel_spin(int profile, float max_omega, float angle)
 	int i;
 	float interval;
 	
-	interval = (PI * angle) / (30.0 * max_omega);
+	interval = fabs((PI * angle) / (30.0 * max_omega));
 	for( i = 0; i < 15; i++)
 	{
 		create_spin((int)(max_omega * speed_profile[profile][i]));
-		sleep(interval - 0.01);
+		//sleep(interval - 0.01);
+		create_wait_duration((int)(interval*10.0));
 	}
 }
 #endif
