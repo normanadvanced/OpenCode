e43ea220a3511cb5ad5aa8375530550102756eaa
Clemens Jung <clemens@jung-baden.at>
2014-07-08 18:07:36 +0200

    changed comment

--- opencode/cbc/drive/drivelib.h
+++ opencode/cbc/drive/drivelib.h
@@ -122,13 +122,17 @@ void cbc_wait(int distance)
 }
 void cbc_halt()
 {
+	// freeze wheels for active stop (cutting power ist inaccurate)
 	set_auto_publish(0); // won't send every command one by one to kmod
 	publish(); // send every enqueued package to kmod
 	freeze(left.wheel.port);
 	freeze(right.wheel.port);
 	set_auto_publish(1); // send every command one by one to kmod
 	publish(); // send enqueued driving commands at once to kmod
-	msleep(100);
+	
+	msleep(100); // wait for bot to stop 
+	
+	// cut motor's power (for saving battery and fitting the rules)
 	set_auto_publish(0); // won't send every command one by one to kmod
 	publish(); // send every enqueued package to kmod
 	off(left.wheel.port);
